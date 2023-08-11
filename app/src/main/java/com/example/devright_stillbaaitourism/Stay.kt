5552d80deb775b7aae1ebffe994d375785fe4eef
Angelo Traverso <atraverso13@gmail.com>
2023-08-11 14:06:31 +0200

    Added temporary icons for drawer menu

--- app/src/main/java/com/example/devright_stillbaaitourism/Stay.kt
+++ app/src/main/java/com/example/devright_stillbaaitourism/Stay.kt
@@ -2,10 +2,29 @@ package com.example.devright_stillbaaitourism
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class Stay : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stay)
+
+
+        val linearLayout = findViewById<LinearLayout>(R.id.linearStayListings);
+        linearLayout.removeAllViews()
+
+        for (i in 1..5)
+        {
+            val customCard = custom_card(this)
+
+            linearLayout.addView(customCard)
+
+        }
+
     }
+
+
 }
