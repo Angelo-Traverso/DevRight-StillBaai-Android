b6fd7250f4d301b662d2d2fe75f07c6982b9c7f4
Angelo Traverso <atraverso13@gmail.com>
2023-09-10 14:01:27 +0200

    Implemented Picasso to dynamically add images from Stilbaai tourism RestAPI

--- app/src/main/java/com/example/devright_stillbaaitourism/CustomAdapter.kt
+++ app/src/main/java/com/example/devright_stillbaaitourism/CustomAdapter.kt
@@ -5,27 +5,36 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
-import com.example.devright_stillbaaitourism.ListItem
+import com.squareup.picasso.Picasso
 import com.example.devright_stillbaaitourism.R
+import com.squareup.picasso.Cache
 
-class CustomAdapter(private val context: Context, private val items: List<ListItem>) : ArrayAdapter<ListItem>(context, 0, items) {
-
+class CustomAdapter(private val context: Context, private val imageUrls: List<String>) : ArrayAdapter<String>(context, 0, imageUrls) {
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         var itemView = convertView
         if (itemView == null) {
+            // Inflate the layout for each list item if it's not already created
             itemView = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false)
         }
 
-        val item = getItem(position)
+        // Get the URL for the current image
+        val imageUrl = getItem(position)
 
+        // Find the ImageView, and TextViews in the list item layout
         val imageView = itemView?.findViewById<ImageView>(R.id.imageView)
         val titleTextView = itemView?.findViewById<TextView>(R.id.titleTextView)
         val descriptionTextView = itemView?.findViewById<TextView>(R.id.descriptionTextView)
 
-        imageView?.setImageResource(item?.thumbnailResId ?: 0)
-        titleTextView?.text = item?.title
-        descriptionTextView?.text = item?.description
+        // Load the image using Picasso from the "rendered" URL
+        Picasso.get().load(imageUrl).into(imageView)
+
+        // Set other TextViews as needed - Should make dynamic
+        // Set the title text
+        titleTextView?.text = "Title"
+        // Set the description text
+        descriptionTextView?.text = "Description"
 
         return itemView ?: View(context)
     }
+
 }
--- app/src/main/java/com/example/devright_stillbaaitourism/Eat.kt
+++ app/src/main/java/com/example/devright_stillbaaitourism/Eat.kt
@@ -12,8 +12,15 @@ import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.devright_stillbaaitourism.databinding.ActivityEatBinding
 import com.google.android.material.navigation.NavigationView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class Eat : AppCompatActivity(), View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var apiService: ApiService
 
     private lateinit var binding: ActivityEatBinding
 
@@ -21,6 +28,19 @@ class Eat : AppCompatActivity(), View.OnClickListener, NavigationView.OnNavigati
         super.onCreate(savedInstanceState)
         binding = ActivityEatBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        // Initialize Retrofit
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://stilbaaitourism.co.za/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        // Create an instance of ApiService
+        apiService = retrofit.create(ApiService::class.java)
+
+        // Make the API request to fetch media items
+        fetchMediaItems()
 
         // ----------------------- List View ----------------------- //
         val listView = findViewById<ListView>(R.id.listView)
@@ -36,22 +56,26 @@ class Eat : AppCompatActivity(), View.OnClickListener, NavigationView.OnNavigati
             ListItem(R.drawable.thumbail, "Item 2", "Description for Item 2"),
         )
 
-        val adapter = CustomAdapter(this, items)
-        listView.adapter = adapter
+
+       /* val adapter = CustomAdapter(this, items)
+        listView.adapter = adapter*/
 
         // Set item click listener for the ListView
+        // Set item click listener for the ListView
         listView.setOnItemClickListener { _, _, position, _ ->
-            val selectedItem = items[position]
+            if (position >= 0 && position < items.size) {
+                val selectedItem = items[position]
 
-            // Create an Intent to open the DetailActivity
-            val intent = Intent(this@Eat, DetailActivity::class.java)
+                // Create an Intent to open the DetailActivity
+                val intent = Intent(this@Eat, DetailActivity::class.java)
 
-            // Pass data to the DetailActivity using Intent extras
-            intent.putExtra("title", selectedItem.title)
-            intent.putExtra("description", selectedItem.description)
-            intent.putExtra("thumbnailResId", selectedItem.thumbnailResId)
+                // Pass data to the DetailActivity using Intent extras
+                intent.putExtra("title", selectedItem.title)
+                intent.putExtra("description", selectedItem.description)
+                intent.putExtra("thumbnailResId", selectedItem.thumbnailResId)
 
-            startActivity(intent)
+                startActivity(intent)
+            }
         }
         // ----------------------- END List View ----------------------- //
 
@@ -66,6 +90,46 @@ class Eat : AppCompatActivity(), View.OnClickListener, NavigationView.OnNavigati
         binding.navView.bringToFront()
         binding.navView.setNavigationItemSelectedListener(this)
     }
+
+    private fun fetchMediaItems() {
+        // Make the API request to fetch media items
+        val call = apiService.getMediaItems()
+        // enqueue() is used to asynchronously make the API request and handle responses
+        call.enqueue(object : Callback<List<MediaItem>> {
+            override fun onResponse(call: Call<List<MediaItem>>, response: Response<List<MediaItem>>) {
+                if (response.isSuccessful) {
+                    // If the API request is successful (HTTP status code 200)
+                    val mediaItems = response.body()
+                    if (mediaItems != null) {
+                        // Extract "thumbnail" URLs from mediaItems
+                        val thumbnailUrls = mediaItems.mapNotNull { it.media_details.sizes["thumbnail"]?.source_url }
+
+                        // Initialize the adapter with the current context and "thumbnail" URLs
+                        val adapter = CustomAdapter(this@Eat, thumbnailUrls)
+
+                        // Find the ListView by its ID
+                        val listView = findViewById<ListView>(R.id.listView)
+
+                        // Set the adapter for the ListView
+                        listView.adapter = adapter
+
+                        // Notify the adapter that the data has changed
+                        adapter.notifyDataSetChanged()
+                    }
+                } else {
+                    // Handle API error
+                }
+            }
+
+            override fun onFailure(call: Call<List<MediaItem>>, t: Throwable) {
+                // If there is a network or other failure in making the API request
+                // Handle network error
+                // Can add network error handling here, such as displaying a network error message
+            }
+        })
+    }
+
+
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation item clicks here
