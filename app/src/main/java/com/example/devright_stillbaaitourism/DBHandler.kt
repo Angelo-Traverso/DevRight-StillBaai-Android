41f95556067de6c5fdf9d9258b5356d56bc976ed
GeoffreyCoding <127305585+GeoffreyCoding@users.noreply.github.com>
2023-09-10 10:34:34 +0200

    DBHandler file initial Commit.

---
+++ app/src/main/java/com/example/devright_stillbaaitourism/DBHandler.kt
@@ -0,0 +1,13 @@
+package com.example.devright_stillbaaitourism
+
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
+
+
+class DBHandler {
+
+    fun connectToDatabase() {
+
+    }
+}
