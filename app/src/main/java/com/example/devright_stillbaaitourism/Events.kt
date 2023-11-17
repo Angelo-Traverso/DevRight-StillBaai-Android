9b9a7533760f29e1f2dbf36d79b86a86391cefb9
Angelo Traverso <atraverso13@gmail.com>
2023-11-17 16:20:20 +0200

    Events display in order of date

--- app/src/main/java/com/example/devright_stillbaaitourism/Events.kt
+++ app/src/main/java/com/example/devright_stillbaaitourism/Events.kt
@@ -1,6 +1,7 @@
 package com.example.devright_stillbaaitourism
 
 import android.content.Intent
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.CalendarContract
@@ -8,93 +9,129 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.squareup.picasso.Picasso
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class Events : AppCompatActivity() {
     private lateinit var burgerMenu: BurgerMenu
     private lateinit var eventList: List<EventData>
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            burgerMenu = BurgerMenu(this, R.layout.activity_events)
-            burgerMenu.setupDrawer()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        burgerMenu = BurgerMenu(this, R.layout.activity_events)
+        burgerMenu.setupDrawer()
 
-            eventList = GlobalClass.EventDataList
+        eventList = GlobalClass.EventDataList
 
-            val linearLayout: LinearLayout = findViewById(R.id.linearEventsListings)
+        val linearLayout: LinearLayout = findViewById(R.id.linearEventsListings)
+        var currentDate: String? = null
 
-            // Loop through your event list and inflate the layout for each event
-            for (event in eventList) {
-                val eventView = layoutInflater.inflate(R.layout.events_custom_layout, null)
 
-                // Set data for each event (assuming you have setter methods in your layout)
-                val eventNameTextView: TextView = eventView.findViewById(R.id.eventNameTextView)
-                val timeTextView: TextView = eventView.findViewById(R.id.timeTextView)
-                //val locationTextView: TextView = eventView.findViewById(R.id.locationTextView)
-                val eventImageView: ImageView = eventView.findViewById(R.id.eventImageView)
-                val addToCalendarButton: Button = eventView.findViewById(R.id.addToCalendarButton)
-                val viewEventDetails: Button = eventView.findViewById(R.id.btnViewEventDetails)
+        // Sort the eventList based on the event dates
+        val sortedEventList = eventList.sortedBy { it.EVENT_DATE }
 
-                eventNameTextView.text = event.EVENT_NAME
+        // Loop through the sorted event list
+        for (event in sortedEventList) {
+            // Check if a new date is encountered
+            if (currentDate != event.EVENT_DATE) {
+                // Add a heading for the new date dynamically
+                val dateHeadingTextView = TextView(this)
+                dateHeadingTextView.text = formatDate(event.EVENT_DATE.toString())
+                dateHeadingTextView.textSize = 24f
+                dateHeadingTextView.setTextColor(ContextCompat.getColor(this, R.color.black))
+                dateHeadingTextView.setTypeface(null, Typeface.BOLD)
+                dateHeadingTextView.setPadding(0, 16, 0, 8)
+                linearLayout.addView(dateHeadingTextView)
+                currentDate = event.EVENT_DATE
 
-                val defaultImageResource = R.drawable.no_image
+                // Iterate through the events with the current date and display them
+                for (eventForDate in sortedEventList.filter { it.EVENT_DATE == currentDate }) {
+                    val eventView = layoutInflater.inflate(R.layout.events_custom_layout, null)
 
-                // Loading images into imageView
-                if (event.EVENT_IMAGE_URLS.isNotEmpty()) {
-                    val imageUrl = event.EVENT_IMAGE_URLS[0]
-                    Picasso.get().load(imageUrl).into(eventImageView)
-                } else {
-                    eventImageView.setImageResource(defaultImageResource)
+                    // Set data for each event (assuming you have setter methods in your layout)
+                    val eventNameTextView: TextView = eventView.findViewById(R.id.eventNameTextView)
+                    val timeTextView: TextView = eventView.findViewById(R.id.timeTextView)
+                    //val locationTextView: TextView = eventView.findViewById(R.id.locationTextView)
+                    val eventImageView: ImageView = eventView.findViewById(R.id.eventImageView)
+                    val addToCalendarButton: Button = eventView.findViewById(R.id.addToCalendarButton)
+                    val viewEventDetails: Button = eventView.findViewById(R.id.btnViewEventDetails)
+
+                    eventNameTextView.text = eventForDate.EVENT_NAME
+
+                    val defaultImageResource = R.drawable.no_image
+
+                    // Loading images into imageView
+                    if (eventForDate.EVENT_IMAGE_URLS.isNotEmpty()) {
+                        val imageUrl = eventForDate.EVENT_IMAGE_URLS[0]
+                        Picasso.get().load(imageUrl).into(eventImageView)
+                    } else {
+                        eventImageView.setImageResource(defaultImageResource)
+                    }
+
+                    timeTextView.text = eventForDate.EVENT_STARTTIME
+
+                    // Set click listener for "Add to Calendar" button
+                    addToCalendarButton.setOnClickListener {
+                        val startTimeMillis = System.currentTimeMillis()
+                        val endTimeMillis = startTimeMillis + 2 * 60 * 60 * 1000
+
+                        val intent = Intent(Intent.ACTION_INSERT)
+                            .setData(CalendarContract.Events.CONTENT_URI)
+                            .putExtra(CalendarContract.Events.TITLE, eventForDate.EVENT_NAME)
+                            .putExtra(CalendarContract.Events.EVENT_LOCATION, eventForDate.EVENT_ADDRESS)
+                            .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, eventForDate.EVENT_STARTTIME)
+                            .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTimeMillis)
+                            .putExtra(CalendarContract.Events.ALL_DAY, false)
+                            .putExtra(CalendarContract.Events.DESCRIPTION, "Reminder")
+
+                        startActivity(intent)
+                    }
+
+                    viewEventDetails.setOnClickListener {
+                        val intent = Intent(this@Events, EventDetail::class.java)
+
+                        val imageUrls = ArrayList(eventForDate.EVENT_IMAGE_URLS)
+
+                        intent.putExtra("eventName", eventForDate.EVENT_NAME)
+                        intent.putExtra("eventNum", eventForDate.EVENT_NUM ?: "")
+                        intent.putExtra("eventEmail", eventForDate.EVENT_EMAIL ?: "")
+                        intent.putExtra("eventWebsite", eventForDate.EVENT_WEBSITE ?: "")
+                        intent.putExtra("eventAddress", eventForDate.EVENT_ADDRESS)
+                        intent.putExtra("eventPerson", eventForDate.EVENT_PERSON ?: "")
+                        intent.putExtra("eventDate", eventForDate.EVENT_DATE ?: "")
+                        intent.putExtra("eventStartTime", eventForDate.EVENT_STARTTIME ?: "")
+                        intent.putExtra("eventDuration", eventForDate.EVENT_DURATION ?: "")
+                        intent.putExtra("eventDescription", eventForDate.EVENT_DESCRIPTION ?: "")
+                        intent.putStringArrayListExtra("eventImageUrls", imageUrls)
+
+                        startActivity(intent)
+                    }
+
+                    val layoutParams = LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                    )
+
+                    layoutParams.setMargins(0, 0, 0, 16)
+                    eventView.layoutParams = layoutParams
+                    // Add the inflated layout to your LinearLayout
+                    linearLayout.addView(eventView)
                 }
-
-                timeTextView.text = event.EVENT_STARTTIME
-                // Set click listener for "Add to Calendar" button
-                addToCalendarButton.setOnClickListener {
-                    val startTimeMillis = System.currentTimeMillis()
-                    val endTimeMillis = startTimeMillis + 2 * 60 * 60 * 1000 // 2 hours
-
-                    val intent = Intent(Intent.ACTION_INSERT)
-                        .setData(CalendarContract.Events.CONTENT_URI)
-                        .putExtra(CalendarContract.Events.TITLE, event.EVENT_NAME)
-                        .putExtra(CalendarContract.Events.EVENT_LOCATION, event.EVENT_ADDRESS)
-                        .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, event.EVENT_STARTTIME)
-                        .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTimeMillis)
-                        .putExtra(CalendarContract.Events.ALL_DAY, false)
-                        .putExtra(CalendarContract.Events.DESCRIPTION, "Reminder")
-
-                    startActivity(intent)
-                }
-
-                viewEventDetails.setOnClickListener{
-                    val intent = Intent(this@Events, EventDetail::class.java)
-
-                    val imageUrls = ArrayList(event.EVENT_IMAGE_URLS)
-
-                    intent.putExtra("eventName", event.EVENT_NAME)
-                    intent.putExtra("eventNum", event.EVENT_NUM ?: "")
-                    intent.putExtra("eventEmail", event.EVENT_EMAIL ?: "")
-                    intent.putExtra("eventWebsite", event.EVENT_WEBSITE ?: "")
-                    intent.putExtra("eventAddress", event.EVENT_ADDRESS)
-                    intent.putExtra("eventPerson", event.EVENT_PERSON ?: "")
-                    intent.putExtra("eventDate", event.EVENT_DATE ?: "")
-                    intent.putExtra("eventStartTime", event.EVENT_STARTTIME ?: "")
-                    intent.putExtra("eventDuration", event.EVENT_DURATION ?: "")
-                    intent.putExtra("eventDescription", event.EVENT_DESCRIPTION ?: "")
-                    intent.putStringArrayListExtra("eventImageUrls", imageUrls)
-
-                    startActivity(intent)
-                }
-
-                val layoutParams = LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT
-                )
-
-                layoutParams.setMargins(0, 0, 0, 16)
-                eventView.layoutParams = layoutParams
-                // Add the inflated layout to your LinearLayout
-                linearLayout.addView(eventView)
             }
         }
-        //............................................................................................//
     }
+
+    private fun formatDate(inputDate: String): String {
+        // Parse the input date string
+        val inputFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        val date = inputFormat.parse(inputDate)
+
+        // Format the date in the desired output format
+        val outputFormat = SimpleDateFormat("EEEE, d MMMM", Locale.getDefault())
+        return outputFormat.format(date ?: Date())
+    }
+    //............................................................................................//
+}
